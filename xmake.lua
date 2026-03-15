18e2e98c9dfb87ea3d974552d94e6aa3cf370d3a
grechka <drvaldemar305@gmail.com>
2026-03-15 20:42:58 +0300

    Fix formatting in xmake.lua for Slog target

--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,6 @@ target("Slog")
     set_kind("static")
     add_files("src/*.cpp")
     add_includedirs("include", {public = true})
-    add_includedirs("src/systems", {public = true}) 
 
     if is_mode("release") then
         add_defines("NDEBUG")
